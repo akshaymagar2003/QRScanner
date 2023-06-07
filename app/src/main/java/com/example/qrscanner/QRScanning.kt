b2fb6c7771c40d4b707da54f19a99ce261a7967e
Akshay@2003 <akshay88magar@gmail.com>
2023-06-07 17:56:14 +0530

    QR Scanning backend updated

--- app/src/main/java/com/example/qrscanner/QRScanning.kt
+++ app/src/main/java/com/example/qrscanner/QRScanning.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.qrscanner.databinding.ActivityQrScanningBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 
 class QRScanning : AppCompatActivity() {
@@ -42,29 +44,39 @@ class QRScanning : AppCompatActivity() {
                 finish()
             }
         }
-        else{
 
-
-
-
+        binding.scanButton.setOnClickListener {
+            // Initiate QR code scan
+            initiateScan()
         }
 
 
 
 
     }
+    private fun initiateScan() {
+        val integrator = IntentIntegrator(this@QRScanning)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Scan a QR Code")
+        integrator.setCameraId(0) // Use the rear camera
+        integrator.setBeepEnabled(false)
+        integrator.setBarcodeImageEnabled(false)
+        integrator.setOrientationLocked(false)
+        integrator.captureActivity = CaptureActivityVertical::class.java
+
+        integrator.initiateScan()
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result: IntentResult? =
+            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null && result.contents != null) {
+            val decodedValue = result.contents
+            binding.decodedTextView.text = decodedValue
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 
 
-//    private fun setText(email:String?)
-//    {
-//        db= FirebaseFirestore.getInstance()
-//        if (email != null) {
-//            db.collection("USERS").document(email).get()
-//                .addOnSuccessListener {
-//                        tasks->
-//
-//                }
-//        }
-//
-//    }
 }
